815381f3626ca0cf9b605d6168b141d0b3b4e64c
olinyavod <olinyavod@gmail.com>
2026-01-25 15:19:49 +1000

    Initial Flutter package structure

---
+++ lib/flutter_easy_sdk.dart
@@ -0,0 +1,5 @@
+/// Flutter SDK with common components for mobile projects.
+library flutter_easy_sdk;
+
+// Export all public APIs here
+export 'src/flutter_easy_sdk_base.dart';
---
+++ lib/src/flutter_easy_sdk_base.dart
@@ -0,0 +1,7 @@
+/// Base class for Flutter Easy SDK.
+/// 
+/// Add your common components here.
+class FlutterEasySdk {
+  /// Returns the current version of the SDK.
+  static const String version = '0.0.1';
+}
